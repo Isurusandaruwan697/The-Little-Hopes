d784df0eae139f5a3324d2f77cd858827bc31e65
Your Name <isurusandaruwan697@gmail.com>
2022-02-23 15:04:00 +0530

    added update on all added member save front and back

--- membersave.php
+++ membersave.php
@@ -3,15 +3,17 @@
 $con = mysqli_connect("localhost", "root", "", "web");
 
 //Set Variebles for the input
-$name = $_POST['name'];
-$ename = $_POST['ename'];
-$bank = $_POST['bank'];
-$cno = $_POST['cno'];
-$valiu  = $_POST['valiu'];
+$name=$_POST['name'];
+$ename=$_POST['ename'];
+$contact=$_POST['contact'];
+$date=date('y-m-d',strtotime($_POST['date']));
+
+$ad=$_POST['ad'];
+$district=$_POST['district'];
 
 
 //Create a MySQL command to INSERT data into data table
-$sql = "INSERT INTO web(name,ename,bank,cno,valiu) VALUES ('$name', '$ename','$bank','$cno','$valiu')";
+$sql = "INSERT INTO info(name,ename,contact,date,ad,district) VALUES ('$name','$ename','$contact','$date','$ad','$district')";
 
 
 //Now Insert data into database
@@ -20,7 +22,7 @@ if (!mysqli_query($con, $sql)) {
 	echo "Data has not saved";
 } else{
 	echo "<script>alert('Wow! User Registration Completed.')</script>";
-	header("Location:http://localhost/web-assesment-group-new/donate.html");}
+	header("Location:http://localhost/web-assesment-group-new/index.html");}
 
 
 
