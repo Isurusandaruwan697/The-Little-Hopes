04c226858e7c940dc24ab4fd72e33018c8a854ab
Your Name <isurusandaruwan697@gmail.com>
2022-02-24 20:58:57 +0530

    sloved the functional problems

--- jscript.js
+++
@@ -1,7 +0,0 @@
-/* for the nav */
-const menubtn=document.querySelector('.menu1')
-const links=document.querySelector('.links')
-
-menubtn.addEventListener('click',()=>{
-links.classList.toggle('menu')
-})
